b32014b719f0bb2c9e6ef288467bcfa001d5c06a
csirianni <cds1@sirianni.org>
2021-03-02 20:37:17 -0500

    make js file for all assignments, not just mountain-paths

---
+++ scripts/assignment.js
@@ -0,0 +1,8 @@
+// Enables source accordion to be opened on click from table of contents
+$('#source-link').click(function(e) {
+    $('#collapseSource').collapse('show');        
+});
+//Enables documentation accordion to be opened on click from table of contents
+$('#documentation-link').click(function(e) {
+    $('#collapseDocumentation').collapse('show');        
+});
