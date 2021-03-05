53627c0afe51b03f4ed6d410f93770305f89fb55
csirianni <cds1@sirianni.org>
2021-03-05 10:02:27 -0500

    delete mountain-path.js file

--- assignments/mountain-paths/scripts/mountain-path.js
+++
@@ -1,8 +0,0 @@
-// Enables source accordion to be opened on click from table of contents
-$('#source-link').click(function(e) {
-    $('#collapseSource').collapse('show');        
-});
-//Enables documentation accordion to be opened on click from table of contents
-$('#documentation-link').click(function(e) {
-    $('#collapseDocumentation').collapse('show');        
-});
